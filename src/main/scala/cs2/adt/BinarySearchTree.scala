c982f726d0daf0c9c34e2673294b5edeed246b6f
Matt Hibbs <mhibbs@trinity.edu>
2021-04-01 14:02:49 -0500

    BST priority queue & tree traversals

--- src/main/scala/cs2/adt/BinarySearchTree.scala
+++ src/main/scala/cs2/adt/BinarySearchTree.scala
@@ -1,6 +1,6 @@
 package cs2.adt
 
-class BinarySearchTree[A <% Ordered[A]] {
+class BinarySearchTree[A <% Ordered[A]] extends Iterable[A] {
   private class Node(var data:A, var left:Node, var right:Node) {
     def contains(elem:A):Boolean = {
       if (data == elem) true
@@ -25,7 +25,7 @@ class BinarySearchTree[A <% Ordered[A]] {
       }
     }
 
-    private def passUpMax():(A,Node) = {
+    def passUpMax():(A,Node) = {
       if(right == null) (data, left)
       else {
         val (d,n) = right.passUpMax()
@@ -51,9 +51,35 @@ class BinarySearchTree[A <% Ordered[A]] {
       }
     }
 
+    def getMax():A = {
+      if(right == null) data
+      else right.getMax()
+    }
+
   }
 
   private var root:Node = null
+
+  def iterator():Iterator[A] = {
+    new Iterator[A] {
+      val stk = Stack[Node]()
+      var curr = root
+
+      def next():A = {
+        while(curr != null) {
+          stk.push(curr)
+          curr = curr.left
+        }
+        curr = stk.pop()
+        val ret = curr.data
+        curr = curr.right
+        ret
+      }
+      def hasNext():Boolean = {
+        curr != null || !stk.isEmpty()
+      }
+    }
+  }
 
   def contains(elem:A):Boolean = {
     if(root == null) false
@@ -69,5 +95,83 @@ class BinarySearchTree[A <% Ordered[A]] {
     if(root != null) root = root.remove(elem)
   }
 
+  def getMax():A = {
+    root.getMax()
+  }
+
+  def removeMax():A = {
+    val (d,n) = root.passUpMax()
+    root = n
+    d
+  }
+
+  override def isEmpty():Boolean = { root == null }
+
+  def printPreOrder():Unit = {
+    def processNode(curr:Node):Unit = {
+      if(curr != null) {
+        print(curr.data + ",")
+        processNode(curr.left)
+        processNode(curr.right)
+      }
+    }
+    processNode(root)
+  }
+
+  def printInOrder():Unit = {
+    def processNode(curr:Node):Unit = {
+      if(curr != null) {
+        processNode(curr.left)
+        print(curr.data + ",")
+        processNode(curr.right)
+      }
+    }
+    processNode(root)
+  }
+
+  def printPostOrder():Unit = {
+    def processNode(curr:Node):Unit = {
+      if(curr != null) {
+        processNode(curr.left)
+        processNode(curr.right)
+        print(curr.data + ",")
+      }
+    }
+    processNode(root)
+  }
+
+  def printPreOrderStack():Unit = {
+    val stk = Stack[Node]()
+    stk.push(root)
+    while(!stk.isEmpty()) {
+      val curr = stk.pop()
+      if(curr != null) {
+        print(curr.data + ",")
+        stk.push(curr.right)
+        stk.push(curr.left)
+      }
+    }
+    println
+  }
+
 
 }
+
+object BinarySearchTree {
+  def main(args:Array[String]):Unit = {
+    var bst = new BinarySearchTree[Int]()
+    for(i <- 1 to 10) {
+      bst.insert(scala.util.Random.nextInt(100))
+    }
+    bst.printPreOrder()
+    println
+    bst.printPreOrderStack()
+    bst.printInOrder()
+    println
+    bst.printPostOrder()
+    println
+
+    for(x <- bst) println(x)
+  }
+}
+
