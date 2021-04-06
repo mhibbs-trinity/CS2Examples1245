1e859d1b54aea857a32c41e2dee2a9c58a1f1367
Matthew Hibbs <mhibbs@trinity.edu>
2021-04-06 14:10:27 -0500

    Heaps and Sets

---
+++ src/main/scala/cs2/adt/Set.scala
@@ -0,0 +1,36 @@
+package cs2.adt
+
+abstract class Set[A <% Ordered[A]] extends Iterable[A] {
+    def add(elem:A):Unit
+    def remove(elem:A):Unit
+    def contains(elem:A):Boolean
+    def size():Int
+
+    def intersect(other:Set[A]):Set[A] = {
+        val res = Set[A]()
+        for(x <- this) {
+            if(other.contains(x)) res.add(x)
+        }
+        res
+    }
+    def union(other:Set[A]):Set[A] = {
+        val res = Set[A]()
+        for(x <- this) res.add(x)
+        for(x <- other) res.add(x)
+        res
+    }
+    def difference(other:Set[A]):Set[A] = {
+        val res = Set[A]()
+        for(x <- this) {
+            if(!other.contains(x)) res.add(x)
+        }
+        res
+    }
+}
+
+object Set {
+    def apply[A <% Ordered[A]]():Set[A] = {
+        new BSTSet[A]()
+    }
+}
+
